a0d4e8f1059ef3ab769ce35ba730caf6799171e0
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-08-11 12:17:31 -0300

    Config teclado e usabilidade SignUp

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -7,6 +7,7 @@ import {
   KeyboardAvoidingView,
   Platform,
   ScrollView,
+  TextInput,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
@@ -24,7 +25,8 @@ import { Container, Title, BackToSign, BackToSignText } from './styles';
 const SignUp: React.FC = () => {
   /* Criando uma Ref para informar ao botão para dar submit, pois no React-Native não temos a funcionalidade de submit no botão */
   const formRef = useRef<FormHandles>(null);
-
+  const emailInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
   const { goBack } = useNavigation();
 
   /* Pedindo para mostrar no console as informações de input quando o botão for clicado e as informações submetidas */
@@ -52,9 +54,53 @@ const SignUp: React.FC = () => {
             <Title>Crie sua conta</Title>
 
             <Form ref={formRef} onSubmit={handleSignIn}>
-              <Input name="name" icon="user" placeholder="Nome" />
-              <Input name="email" icon="mail" placeholder="E-mail" />
-              <Input name="password" icon="lock" placeholder="Senha" />
+              <Input
+                name="name"
+                icon="user"
+                placeholder="Nome"
+                /* Deixa em caixa alta apenas a primeira letra de cada uma das palavras */
+                autoCapitalize="words"
+                /* Tirando a correção automática de texto */
+                autoCorrect={false}
+                /* Mudando o botão enter do teclado para next */
+                returnKeyType="next"
+                /* Quando clicar no botão do teclado enter, ele chama a função para passar ao próximo campo, independente do formato do botão, utilizamos as Refs pra isso */
+                onSubmitEditing={() => {
+                  emailInputRef.current?.focus();
+                }}
+              />
+              <Input
+                ref={emailInputRef}
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                /* Tirando a correção automática de texto */
+                autoCorrect={false}
+                /* Tira a caixa alta automática no início de cada palavra */
+                autoCapitalize="none"
+                /* Trocando o tipo de teclado para esse input */
+                keyboardType="email-address"
+                /* Mudando o botão enter do teclado para next */
+                returnKeyType="next"
+                /* Quando clicar no botão do teclado enter, ele chama a função para passar ao próximo campo, independente do formato do botão, utilizamos as Refs pra isso */
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus();
+                }}
+              />
+              <Input
+                ref={passwordInputRef}
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                /* Escodendo a senha */
+                secureTextEntry
+                /* Mudando o botão enter do teclado para send(enviar) */
+                returnKeyType="send"
+                /* Quando clicar no botão do teclado enter, ele chama a função para fazer o submit, independente do formato do botão */
+                onSubmitEditing={() => {
+                  formRef.current?.submitForm();
+                }}
+              />
             </Form>
 
             <Button
